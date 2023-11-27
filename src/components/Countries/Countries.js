cd418eccc97c5f2af6b292a6a8814706a983776c
Ripon Mondal <riponmondal149@gmail.com>
2023-11-28 02:45:57 +0600

    changed more

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+
     return (
         <div>
-            <h2>Countries in other File</h2>
+            <h1>Hello from countries: {countries.length}</h1>
+            {
+                countries.map(country => <Country
+                    name={country.name.common}
+                    population={country.population}
+                    area={country.area}
+                ></Country>)
+            }
         </div>
     );
 };
